18f4a368eee2089bc8a26c95d0c97102d06e1604
jay finaviya <jayfinaviya5@gmail.com>
2025-11-30 21:54:42 +0530

    first coomit

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,5 +1,9 @@
 const Listing = require("../models/listing");
 
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
@@ -28,6 +32,16 @@ module.exports.showListing = async(req,res)=>{
 };
 
 module.exports.createListing = async (req, res) => {
+
+let response = await geocodingClient.forwardGeocode({
+  query: req.body.listing.location,
+  limit: 1,
+})
+  .send();
+  console.log(response.body.features[0].geometry);
+  res.send("done!");
+  
+
     const listingData = req.body.listing || {};
     let url = req.file.path;
     let filename = req.file.filename;
