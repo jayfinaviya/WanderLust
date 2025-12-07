47d2ec27efc91c1c6ced02e5609df4efc3a441ad
jay finaviya <jayfinaviya5@gmail.com>
2025-12-07 23:07:02 +0530

    first coomit

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ const User = require("./models/user.js");
 const listingRouter = require("./routes/listing.js");
 const reviewRouter  = require("./routes/review.js");
 const userRouter  = require("./routes/user.js");
-const { required } = require("joi");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
 .then(()=>{
@@ -31,7 +32,7 @@ main()
 });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine","ejs");
